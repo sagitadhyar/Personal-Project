e8d8056f1ff93874be10a72669d1f21be155f0cf
sagitadhyar <sagita1900016189@webmail.uad.ac.id>
2021-07-22 14:40:44 +0700

    Done UI Forgot Password

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { Constants } from 'src/app/common/constants';
 
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
+  title = Constants.siteTitle
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  matcher = new MyErrorStateMatcher();
 
 }
